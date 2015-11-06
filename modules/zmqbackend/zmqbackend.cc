056fc48ce7a6e2dd21d70b8262b6c5d2d7a36f4c
dewey hong <doohee323@gmail.com>
2015-11-06 13:58:23 -0800

    make zmqbackend

--- modules/zmqbackend/zmqbackend.cc
+++ modules/zmqbackend/zmqbackend.cc
@@ -105,7 +105,32 @@ ZMQBackend::~ZMQBackend()
 //
 void ZMQBackend::send(const string &line)
 {
+	//
+	// send to zmq socket
+	// blocking send
+	// if the socket is not in the right mode to send, a zmq::error_t (std::exception)
+	// 	will be thrown and we should attempt to reconnect and re-send
+	//
+	if (!zmq_socket)
+	{
+		throw ZException("can't call send on a null zmq_socket");
+	}
 
+	try
+	{
+		//
+		// returns false on EAGAIN from socket, which shouldn't ever occur
+		// since we're not using nonblocking sockets...
+		//
+		if (!zmq::s_send(*zmq_socket, line))
+		{
+			throw ZException("send returned false and we're not using nonblocking sockets");
+		}
+	}
+	catch (const std::exception &e)
+	{
+		throw ZException(e.what());
+	}
 }
 
 //
@@ -124,6 +149,40 @@ void ZMQBackend::send(const string &line)
 //
 void ZMQBackend::receive(string &line)
 {
+	try
+	{
+		//
+		// receive line from zmq socket
+		//
+		zmq::pollitem_t items[] =
+		{
+			{ *zmq_socket, 0, ZMQ_POLLIN, 0 }
+		};
+		zmq::poll(&items[0], 1, (long)zmq_timeout);
+
+		if (items[0].revents & ZMQ_POLLIN)
+		{
+			line = zmq::s_recv(*zmq_socket);
+			return;
+		}
+	}
+	catch (const std::exception &e)
+	{
+		L << Logger::Error << kBackendId << " caught exception in receive: " << e.what() << endl;
+		throw ZException(e.what());
+	}
+
+	//
+	// got here because no response within timeout seconds, throw
+	// exception on assumption that the backend is foobar or the
+	// socket is blown, so we'll restart a new backend and thus a new
+	// connection.
+	//
+	// it is wise to set timeout high enough that you don't tear down
+	// and set up new connections continuously, but not so high that
+	// you give poor performance.
+	//
+	throw ZException("socket timeout");
 }
 
 //
@@ -138,12 +197,236 @@ void ZMQBackend::receive(string &line)
 //	any calls to get() will return false if that has happened,
 //	and if the call to lookup() succeeded, get() will access the results in $lineQueue
 //
-void ZMQBackend::lookup( const QType& qtype, const DNSName& qname, DNSPacket* dnspkt, int zoneid )
+void ZMQBackend::lookup( const QType& qtype, const DNSName& qname, DNSPacket* pkt_p, int zoneId )
 {
+	//
+	// reset all state variables to a clean state and disconnect
+	// any previously connected socket and delete any prior zmq context
+	//
+	while (!lineQueue.empty())
+	{
+		lineQueue.pop();
+	}
+	
+	//
+	// there's no way to turn off wildcard support in pdns itself, so we have to intercept it here
+	//
+	if (!do_wildcards && qname.toString().compare("*"))
+	{
+		return;
+	}
+
+	//
+	// if ! connected, connect
+	//
+
+	if (NULL == zmq_socket)
+	{
+		try
+		{
+			//
+			// this end of the zmq socket is a ZMQ_REQ socket, the other should be
+			// a ROUTER socket or a REP socket.
+			//
+			zmq_socket  = new zmq::socket_t(*zmq_context, ZMQ_REQ);
+	
+			uint64_t hwm = 1;
+			int linger_time = 1;
+	
+			zmq_socket->setsockopt(ZMQ_HWM, &hwm, sizeof(hwm));
+			zmq_socket->setsockopt(ZMQ_LINGER, &linger_time, sizeof(linger_time));
+	
+			zmq_socket->connect(zmq_url.c_str());
+	
+			//
+			// leaving zmq-version in to support versioning of backends
+			//
+			send("HELO\t" + getArg("version"));
+			string banner = "";
+			receive(banner);
+	
+			if (banner != "OK")
+			{
+				throw ZException("version disagreement with backend, it does not like version " + getArg("version"));
+			}
+	
+			L << Logger::Info << "Backend " << suffix << " launched with banner: " << banner << endl;
+		}
+		catch (const ArgException &A)
+		{
+			L << Logger::Error << kBackendId << " Fatal argument error: " << A.reason << endl;
+			if (zmq_socket)
+			{
+				zmq_socket->close();
+				delete zmq_socket;
+				zmq_socket = NULL;
+			}
+			return;
+		}
+		catch (const std::exception &e)
+		{
+			L << Logger::Error << kBackendId << " Fatal argument error: " << e.what() << endl;
+			if (zmq_socket)
+			{
+				zmq_socket->close();
+				delete zmq_socket;
+				zmq_socket = NULL;
+			}
+			return;
+		}
+		catch (ZException &e)
+		{
+			L << Logger::Error << kBackendId << " Fatal argument error: " << e.what() << endl;
+			if (zmq_socket)
+			{
+				zmq_socket->close();
+				delete zmq_socket;
+				zmq_socket = NULL;
+			}
+			return;
+		}
+	}
+
+	//
+	// we are now connected or we have returned and lineQueue is empty
+	//
+
+	string localIP  = "0.0.0.0";
+	string remoteIP = "0.0.0.0";
+	Netmask realRemote("0.0.0.0/0");
+
+	if (pkt_p)
+	{
+		localIP    = pkt_p->getLocal();
+		remoteIP   = pkt_p->getRemote();
+		realRemote = pkt_p->getRealRemote();
+	}
+
+	//
+	// zmq-version: 1
+	//
+	// qname	   qtype   	id      remote-ip-address	local-ip	realRemoteIP
+	// $hostname	   A|MX|ANY|etc	id	remote ip address	local ip	remote ip address
+	//
+	// zmq-version: 2
+	//
+	// qname	   qtype   	id      remote-ip-address	local-ip	realRemoteIP		real-request-type
+	// $hostname	   A|MX|ANY|etc	id	remote ip address	local ip	remote ip address	ANY, CNAME, AAAA, A, MX, etc
+	//
+
+	ostringstream query;
+
+	if (version == 1)
+	{
+		query << qname << "\t" << qtype.getName() << "\t" << zoneId << "\t" << remoteIP << "\t" << localIP << "\t" << realRemote.toString();
+	}
+	else if (version == 2)
+	{
+		query << qname << "\t" << qtype.getName() << "\t" << zoneId << "\t" << remoteIP << "\t" << localIP << "\t" << realRemote.toString()
+			<< "\t" << pkt_p->qtype.getName();
+	}
+
+	if (do_query_logging)
+	{
+		L << Logger::Debug << "Query: '" << query.str() << "'" << endl;
+	}
+
+	d_qtype = qtype;
+	d_qname = qname.toString();
+
+	try
+	{
+		string response;
+
+		send(query.str());
+		receive(response);
+
+		//
+		// receive throws an exception on any error or failure, including socket timeout.
+		// if we got HERE, it means there was no problem receiving and we should have something
+		// in $response.
+		// for the case where the client (us) gets in a bad state because the server (backend)
+		// was restarted, timing out, throwing an exception, and closing the socket to teh server
+		// in the exception handling code is a good way to work around that, because zmq won't
+		// get our socket back into the right state again by itself.
+		//
+
+		DLOG(L << Logger::Debug << kBackendId << " backend returned response: " << response << endl);
+	
+		if (response == "")
+		{
+			L << Logger::Error << kBackendId << " backend returned empty line in query for " << d_qname << endl;
+			throw ZException("Format error communicating with backend");
+		}
+
+		//
+		// populate the lineQueue for get()
+		//
+		vector<string> lines;
+		stringtok(lines, response, "\n");
+	
+		BOOST_FOREACH(const string& line, lines)
+		{
+			lineQueue.push(line);
+		}
+	}
+	catch (ZException &e)
+	{
+		L << Logger::Info << "error sending query to backend: " << e.what() << endl;
+		if (zmq_socket)
+		{
+			zmq_socket->close();
+			delete zmq_socket;
+			zmq_socket = NULL;
+		}
+		return;
+	}
+	catch (const std::exception &e)
+	{
+		L << Logger::Info << "error sending query to backend: " << e.what() << endl;
+		if (zmq_socket)
+		{
+			zmq_socket->close();
+			delete zmq_socket;
+			zmq_socket = NULL;
+		}
+		return;
+	}
+	catch (...)
+	{
+		L << Logger::Info << "unknown exception caught for error sending query to backend" << endl;
+		if (zmq_socket)
+		{
+			zmq_socket->close();
+			delete zmq_socket;
+			zmq_socket = NULL;
+		}
+		return;
+	}
 }
 
 bool ZMQBackend::getSOA(const DNSName& name, SOAData& soadata, DNSPacket*)
 {
+	if (name.empty())
+	{
+		return false;
+	}
+
+	soadata.domain_id = 1;
+
+	soadata.nameserver = DNSName("r1.3crowd.com");
+	soadata.hostmaster = DNSName("hostmaster.r1.3crowd.com");
+	soadata.serial = 3;
+
+	soadata.refresh = 60;
+	soadata.retry = 60;
+	soadata.expire = 60;
+	soadata.ttl  = 60;
+	soadata.default_ttl = 60;
+
+	soadata.db = this;
+	soadata.scopeMask = (uint8_t)0;
+
 	return true;
 }
 
@@ -157,8 +440,120 @@ bool ZMQBackend::getSOA(const DNSName& name, SOAData& soadata, DNSPacket*)
 // throws:
 //	nothing
 //
-bool ZMQBackend::get(DNSResourceRecord &rr)
+bool ZMQBackend::get(DNSResourceRecord &r)
 {
+	if (lineQueue.empty())
+	{
+		return false;
+	}
+
+	//
+	// iterate over lines until we find a good one or END
+	//
+	while (!lineQueue.empty())
+	{
+		string line = lineQueue.front();
+		lineQueue.pop();
+	
+		DLOG(L << Logger::Debug << "ZMQBackend::get got line: " << line << endl);
+	
+		//
+		// The answer format:
+		// DATA    qname	   qclass  qtype   ttl     id      content
+		//
+	
+		vector<string>parts;
+		stringtok(parts, line, "\t");
+	
+		if (parts.empty())
+		{
+			L << Logger::Info << kBackendId << " backend returned empty line in query for " << d_qname << endl;
+			continue;
+		}
+		else if (parts[0] == "END")
+		{
+			return false;
+		}
+		else if (parts[0] == "DATA")   // yay
+		{
+			if (parts.size() < 9)
+			{
+				L << Logger::Info << kBackendId << " backend returned incomplete or empty line in data section for query for " << d_qname << endl;
+				continue;
+			}
+	
+			/*
+			 * Fields list:
+			 *
+	
+			0: DATA
+			1: scopeMask - used for edns subnet responses
+			2: auth
+			3: qname
+			4:
+			5: qtype
+			6: ttl
+			7: domain_id
+	
+			If MX|SRV:
+	
+			8: priority
+			9: content
+	
+			else:
+	
+			8...: content
+		
+			*/
+	
+			//
+			// DATA		= parts[0];
+			//
+			r.scopeMask	= atoi(parts[1].c_str());
+			r.auth		= atoi(parts[2].c_str());
+			r.qname		= DNSName(parts[3]);
+			//
+			// IN		= parts[4];
+			//
+			r.qtype		= parts[5];
+			r.ttl		= atoi(parts[6].c_str());
+			r.domain_id	= atoi(parts[7].c_str());
+		
+			if (r.qtype.getCode() == QType::MX || r.qtype.getCode() == QType::SRV)
+			{
+				if (parts.size() < 10)
+				{
+					L << Logger::Info << kBackendId << " backend returned incomplete MX/SRV line in data section for query for " << d_qname << endl;
+					continue;
+				}
+	
+				// r.priority = atoi(parts[8].c_str());
+				r.content = parts[9];
+			}
+			else
+			{
+				r.content.clear();
+	
+				for (unsigned int n = 8; n < parts.size(); ++n)
+				{
+					if (n != 8)
+					{
+						r.content.append(1, ' ');
+					}
+					r.content.append(parts[n]);
+				}
+			}
+
+			return true;
+		}
+		else
+		{
+			L << Logger::Info << "ZMQprocess backend sent incorrect response '" << line << "'";
+			continue;
+		}
+	}
+	
+	L << Logger::Info << "ran out of lines to process while looking for a good response for query " << d_qname << endl;
 	return false;
 }
 	
@@ -171,8 +566,13 @@ bool ZMQBackend::get(DNSResourceRecord &rr)
 // throws:
 // 	nada
 //
-bool ZMQBackend::list( const DNSName& target, int zoneid, bool include_disabled )
+bool ZMQBackend::list( const DNSName& target, int inZoneId, bool include_disabled )
 {
+	//
+	// The question format:
+	// qname	   qtype   id      ip-address	local-ip	edns-subnet remote subnet
+	//
+	d_qname = itoa(inZoneId);
 	return true;
 }
 
@@ -222,7 +622,7 @@ class ZMQLoader
 		{
 			BackendMakers().report(new ZMQFactory);
 
-			//L << Logger::Notice << kBackendId << " This is the zmqbackend version "VERSION" ("__DATE__", "__TIME__") reporting"; << endl;
+			//L << Logger::Notice << kBackendId << " This is the zmqbackend version "VERSION" ("__DATE__", "__TIME__") reporting" << endl;
 		}
 };
 
